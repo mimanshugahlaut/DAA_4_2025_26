70203b52206f54e6d680d2461ad8c643b1b8858d
Mimanshu Gahlaut <mimanshugahlaut10@gmail.com>
2026-03-06 23:01:16 +0530

    Implement longest common subsequence algorithm

---
+++ Session-18/lcs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int a=text1.length();
+        int b=text2.length();
+      
+
+        if(a==0||b==0) return 0;
+
+        vector<vector<int>> dp(a+1,vector<int> (b+1,0));
+
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=b;j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                   
+                }
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+       
+        return dp[a][b];
+    }
+};
